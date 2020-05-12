e96964f1beb05ee2ecfe77b495b52da2498f4914
artemkoorochka <artem@koorochka.com>
2020-05-13 00:09:34 +0300

    Unity C# уроки / #4 - Отслеживание нажатий от пользователя Primitive Horizontal move

--- scripts/Assets/scrpt.cs
+++ scripts/Assets/scrpt.cs
@@ -6,56 +6,13 @@ public class scrpt : MonoBehaviour
 {
     
     public GameObject obj;
-    private Light myLight;
-    private int numEnimies = 10;
-
-    private void Start()
-    {
-        // При старте присваиваем нашей переменной обект с юнити Directional Light
-        myLight = GetComponent<Light>();
-
-        // cicle
-        for (int i = 0; i < numEnimies; i++)
-        {
-            Debug.Log("Create " + i + " enimis");
-        }
-    }
+    public float range = 5f;
 
     private void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            myLight.enabled = !myLight.enabled;
-        }
+        float h = Input.GetAxis("Horizontal");
+        float xpos = h * range;
 
-        if(Input.GetKeyUp(KeyCode.A))
-        {
-            obj.SetActive(false);
-        }
-
-        if(Input.GetKeyUp(KeyCode.S))
-        {
-            obj.SetActive(true);
-        }
-
-        if (Input.GetKeyUp(KeyCode.Delete))
-        {
-            Destroy(obj);
-        }
-
-        // RGB color
-        if(Input.GetKeyUp(KeyCode.R))
-        {
-            obj.GetComponent<Renderer>().material.color = Color.red;
-        }
-        if(Input.GetKeyUp(KeyCode.G))
-        {
-            obj.GetComponent<Renderer>().material.color = Color.green;
-        }
-        if(Input.GetKeyUp(KeyCode.B))
-        {
-            obj.GetComponent<Renderer>().material.color = Color.blue;
-        }
-
+        obj.transform.position = new Vector3(xpos, 0, 0);
     }
 }
