d59dfcf45aa56530f4d8d2096f98a019649fb081
artemkoorochka <artem@koorochka.com>
2020-05-13 00:34:09 +0300

    Unity C# уроки / #4 - Отслеживание нажатий от пользователя

---
+++ scripts/Assets/scale.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scale : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        transform.localScale = new Vector3(transform.localScale.x / 2, transform.localScale.y / 2, transform.localScale.z / 2);
+    }
+}
--- scripts/Assets/scrpt.cs
+++ scripts/Assets/scrpt.cs
@@ -6,13 +6,20 @@ public class scrpt : MonoBehaviour
 {
     
     public GameObject obj;
-    public float range = 5f;
+    public float range = 5f, moveSpeed = 5f, turnSpeed = 40f;
 
     private void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        float xpos = h * range;
+        if (Input.GetKey(KeyCode.UpArrow))
+            obj.transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
 
-        obj.transform.position = new Vector3(xpos, 0, 0);
+        if (Input.GetKey(KeyCode.DownArrow))
+            obj.transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            obj.transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            obj.transform.Rotate(Vector3.down, turnSpeed * Time.deltaTime);
     }
 }
