1b5374e31925af92d3914e1ac184cd4cf4163f0c
artemkoorochka <artem@koorochka.com>
2020-05-13 00:42:17 +0300

    Simple Instantiate position by object

--- scripts/Assets/scrpt.cs
+++ scripts/Assets/scrpt.cs
@@ -5,21 +5,11 @@ using UnityEngine;
 public class scrpt : MonoBehaviour
 {
     
-    public GameObject obj;
-    public float range = 5f, moveSpeed = 5f, turnSpeed = 40f;
+    public GameObject[] objects;
 
-    private void Update()
+    private void Start()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
-            obj.transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
-
-        if (Input.GetKey(KeyCode.DownArrow))
-            obj.transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
-
-        if (Input.GetKey(KeyCode.LeftArrow))
-            obj.transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
-
-        if (Input.GetKey(KeyCode.RightArrow))
-            obj.transform.Rotate(Vector3.down, turnSpeed * Time.deltaTime);
+        Instantiate(objects[2], objects[2].transform.position);
     }
+
 }
