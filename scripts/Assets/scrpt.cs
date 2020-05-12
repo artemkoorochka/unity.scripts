88cbdba5677ca8031879a234a82fba19821362ef
artemkoorochka <artem@koorochka.com>
2020-05-12 22:32:12 +0300

    Simple awake start and update functions/ Out to console

--- scripts/Assets/scrpt.cs
+++ scripts/Assets/scrpt.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class scrpt : MonoBehaviour
 {
+    private void Awake()
+    {
+        print("Awake work before Start");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("Hi");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        print("Game Upadate");
     }
 }
