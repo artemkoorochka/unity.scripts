1c3d122bf112e3f538e0695e8c0c4316a5adaf43
artemkoorochka <artem@koorochka.com>
2020-05-13 01:02:17 +0300

    Instantiate Создание объекта и работа с ним

--- scripts/Assets/scrpt.cs
+++ scripts/Assets/scrpt.cs
@@ -6,11 +6,13 @@ public class scrpt : MonoBehaviour
 {
     
     public GameObject[] objects;
+    private GameObject instObject;
 
     private void Start()
     {
-        int rand = Random.Range(0, objects.Length);
-        Instantiate(objects[rand], objects[rand].transform.position, Quaternion.identity);
+        int rand = Random.Range(0, objects.Length - 1);
+        instObject = Instantiate(objects[rand], objects[rand].transform.position, Quaternion.identity) as GameObject;
+        instObject.transform.localScale = new Vector3(0.24f, 0.24f, 0.24f);
     }
 
 }
