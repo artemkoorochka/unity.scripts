43e09f2e69c8de2383fa4767a56d94091a997608
artemkoorochka <artem@koorochka.com>
2020-05-13 20:26:17 +0300

    GameObject.Find sample

--- scripts/Assets/scale.cs
+++ scripts/Assets/scale.cs
@@ -1,9 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class scale : MonoBehaviour
 {
+    private Text txt;
+    private int count;
+
+    private void Start()
+    {
+        txt = GameObject.Find("Text").GetComponent<Text>();
+    }
+
     private void OnMouseDown()
     {
         transform.localScale = new Vector3(transform.localScale.x / 2, transform.localScale.y / 2, transform.localScale.z / 2);
@@ -11,6 +20,7 @@ public class scale : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        print(other.gameObject.name);
+        count++;
+        txt.text = other.gameObject.name + " " + count.ToString();
     }
 }
