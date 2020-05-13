8803c1aa6ab19daca58585fb7d798b22d4d161c7
artemkoorochka <artem@koorochka.com>
2020-05-13 18:26:10 +0300

    Unity C# уроки / #6 - Корутины (Coroutines) Invoke sample

--- scripts/Assets/scrpt.cs
+++ scripts/Assets/scrpt.cs
@@ -7,6 +7,11 @@ public class scrpt : MonoBehaviour
     
     public GameObject obj;
 
+    private void Start()
+    {
+        Invoke("Inst", 2f);
+    }
+
     private void Update()
     {
         if (Input.GetKey(KeyCode.F))
@@ -16,10 +21,16 @@ public class scrpt : MonoBehaviour
     }
 
     IEnumerator instObj () {
-        yield return new WaitForSeconds(1.5f);
-        Instantiate(obj, obj.transform.position, Quaternion.identity);
+        while (true)
+        {
+            Instantiate(obj, new Vector3(Random.Range(-10f, 10f), 0f, Random.Range(-10f, 10f)), Quaternion.identity);
+            yield return new WaitForSeconds(0.5f);
+        }
     }
 
-
+    void Inst()
+    {
+        Instantiate(obj, new Vector3(Random.Range(-10f, 10f), 0f, Random.Range(-10f, 10f)), Quaternion.identity);
+    }
 
 }
